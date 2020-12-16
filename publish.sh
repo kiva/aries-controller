1c748b0c76616946b730b136b8b4824650529948
Jacob Saur <jsaur@kiva.org>
2020-12-16 14:28:49 +0100

    attempt publishing dev package
    Signed-off-by: Jacob Saur <jsaur@kiva.org>

--- publish.sh
+++ publish.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 set -ex
+if [[ $1 == "dev" ]]
+then
+  opt=" --tag dev" 
+else
+  opt="" 
+fi
 
 cp package.json dist/
 cp README.md dist/
 cp .npmrc dist/
 cd dist
-npm publish --verbose
+npm publish --verbose $opt
 cd ..
