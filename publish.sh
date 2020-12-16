1747ad6ab0a1543ebac83bccf60adb1342d56861
Jacob Saur <jsaur@kiva.org>
2020-12-16 14:42:43 +0100

    that didn't work
    Signed-off-by: Jacob Saur <jsaur@kiva.org>

--- publish.sh
+++ publish.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
 set -ex
-if [[ $1 == "dev" ]]
-then
-  opt=" --tag dev" 
-else
-  opt="" 
-fi
 
 cp package.json dist/
 cp README.md dist/
 cp .npmrc dist/
 cd dist
-npm publish --verbose $opt
+npm publish --verbose
 cd ..
