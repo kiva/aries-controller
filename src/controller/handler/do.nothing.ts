91823bc5c80552a927ca780be1027ab3165e14c3
Jacob Saur <jsaur@kiva.org>
2021-03-03 19:41:58 +0100

    change to do nothing
    Signed-off-by: Jacob Saur <jsaur@kiva.org>

--- src/controller/handler/do.nothing.ts
+++ src/controller/handler/do.nothing.ts
@@ -4,13 +4,16 @@ import { Logger } from 'protocol-common/logger';
 import { IAgentResponseHandler } from './agent.response.handler';
 import { AgentGovernance } from '../agent.governance';
 
-
-export class RevocationRegistry implements IAgentResponseHandler {
+/**
+ * This handler is for any topics that don't require any special handling
+ * We just log a debug message so we can see what's being called if we need to
+ */
+export class DoNothing implements IAgentResponseHandler {
     constructor(private readonly agentGovernance: AgentGovernance, private readonly http: ProtocolHttpService, private readonly cache: CacheStore) {
     }
 
 public async handlePost(agentUrl: string, agentId: string, adminApiKey: string, route: string, topic: string, body: any): Promise<any> {
-        Logger.warn(`Doing nothing in RevocationRegistry for '${agentId}': ${JSON.stringify(body)}`);
+        Logger.warn(`Doing nothing for ${topic} for '${agentId}': ${JSON.stringify(body)}`);
         return 'ok';
     }
 }
