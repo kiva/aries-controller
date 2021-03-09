d473e5eec489449ee49d7780f8922867a0b8b0c6
Cam Parra <caeparra@gmail.com>
2021-03-09 16:52:27 -0700

    makes variable names more consistent
    Signed-off-by: Cam Parra <caeparra@gmail.com>

--- src/issuer/issuer.service.ts
+++ src/issuer/issuer.service.ts
@@ -233,9 +233,9 @@ export class IssuerService {
     /**
      * Revokes credential using the cred_rev_id and rev_reg_id
      */
-    public async revokeCredential(cred_ex_id : number, publish : boolean): Promise<any> {
+    public async revokeCredential(credential_exchange_id : number, publish : boolean): Promise<any> {
         const data = {
-            cred_ex_id,
+            cred_ex_id: credential_exchange_id,
             publish,
         };
         return await this.agentCaller.callAgent(process.env.AGENT_ID, process.env.ADMIN_API_KEY, 'POST', 'revocation/revoke', null, data);
