a1f473f1473c001da35ff046f5014a2d5ca6c85b
Cam Parra <caeparra@gmail.com>
2021-04-19 16:39:39 -0600

    fix delete function"
    Signed-off-by: Cam Parra <caeparra@gmail.com>

--- src/agent/agent.service.ts
+++ src/agent/agent.service.ts
@@ -81,11 +81,8 @@ export class AgentService {
     /**
      * Deletes credential using the cred_rev_id for issuer
      */
-     public async deleteCredential(credential_id : string): Promise<any> {
-        const data = {
-            credential_id,
-        };
-        return await this.agentCaller.callAgent(process.env.AGENT_ID, process.env.ADMIN_API_KEY, 'DELETE', 'credential/records', null, data);
+    public async deleteCredential(credentialExchangeI: string): Promise<any> {
+        return await this.agentCaller.callAgent(process.env.AGENT_ID, process.env.ADMIN_API_KEY, 'DELETE', `credential/records/${credentialExchangeI}`);
     }
 
     /**
