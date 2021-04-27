5bde1fffc982c9a43387ca1c93bd28c696732401
Matt Raffel <mattr@kiva.org>
2021-04-27 10:30:28 -0400

    PR changes
    Signed-off-by: Matt Raffel <mattr@kiva.org>

--- src/controller/agent.governance.factory.ts
+++ src/controller/agent.governance.factory.ts
@@ -1,18 +1,18 @@
 import { AgentGovernance } from './agent.governance';
 
 
-const governance = new Map<string, AgentGovernance>();
+const governanceMap = new Map<string, AgentGovernance>();
 
 export const AgentGovernanceFactory = {
     provide: 'AGENT_GOVERNANCE',
     useFactory: () => {
         const policyName = process.env.POLICY_NAME;
-        if (governance.has(policyName)) {
-            return governance.get(policyName);
+        if (governanceMap.has(policyName)) {
+            return governanceMap.get(policyName);
         }
 
-        const handler: AgentGovernance = new AgentGovernance(policyName);
-        governance.set(policyName, handler);
-        return handler;
+        const governanceInstance: AgentGovernance = new AgentGovernance(policyName);
+        governanceMap.set(policyName, governanceInstance);
+        return governanceInstance;
     },
 };
