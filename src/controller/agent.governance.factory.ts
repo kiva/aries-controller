ec0c8b67dce5e5dc64dbc203125ed540da2aa70e
Matt Raffel <mattr@kiva.org>
2021-04-27 09:46:59 -0400

    makes AgentGovernence multiton (aka several singletons)
    Signed-off-by: Matt Raffel <mattr@kiva.org>

--- src/controller/agent.governance.factory.ts
+++ src/controller/agent.governance.factory.ts
@@ -1,8 +1,19 @@
 import { AgentGovernance } from './agent.governance';
 
+
+const governance = new Map<string, AgentGovernance>();
+
 export const AgentGovernanceFactory = {
     provide: 'AGENT_GOVERNANCE',
     useFactory: () => {
-        return new AgentGovernance(process.env.POLICY_NAME);
+        // return new AgentGovernance(process.env.POLICY_NAME);
+
+        const policyName = process.env.POLICY_NAME;
+        if (governance.has(policyName))
+            return governance[policyName].value;
+
+        const handler: AgentGovernance = new AgentGovernance(policyName);
+        governance.set(policyName, handler);
+        return handler;
     },
 };
