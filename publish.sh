ad4a11d39d160b53f65d2b22d9ee2afb75187f4c
Jacob Saur <jsaur@kiva.org>
2021-05-03 11:19:34 +0000

    remove npmrc except for on publish
    Signed-off-by: Jacob Saur <jsaur@kiva.org>

--- publish.sh
+++ publish.sh
@@ -3,7 +3,8 @@ set -ex
 
 cp package.json dist/
 cp README.md dist/
-cp .npmrc dist/
+# The .npmrc file is only needed for publish and can cause issues when left around, so manually adding here
+echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > dist/.npmrc
 cd dist
 npm publish --verbose
 cd ..
