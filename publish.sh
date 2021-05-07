cb3f4dac765fb56b59b36a0166292c1427984135
Jacob Saur <jsaur@kiva.org>
2021-05-07 13:49:33 +0000

    dont accidentally publish token
    Signed-off-by: Jacob Saur <jsaur@kiva.org>

--- publish.sh
+++ publish.sh
@@ -4,7 +4,7 @@ set -ex
 cp package.json dist/
 cp README.md dist/
 # The .npmrc file is only needed for publish and can cause issues when left around, so manually adding here
-echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > dist/.npmrc
+echo '//registry.npmjs.org/:_authToken=${NPM_TOKEN}' > dist/.npmrc
 cd dist
 npm publish --verbose
 cd ..
