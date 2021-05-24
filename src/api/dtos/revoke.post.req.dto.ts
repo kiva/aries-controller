0ab94c00a148a1bdd90178ba2721108445e02012
Cam Parra <51462259+mac-arrap@users.noreply.github.com>
2021-05-24 08:33:42 -0600

    add dto to revoke endpoint (#68)
    * add dto to revoke endpoint
    
    Signed-off-by: Cam Parra <caeparra@gmail.com>
    
    * delete dto to revoke endpoint
    
    Signed-off-by: Cam Parra <caeparra@gmail.com>
    
    * delete dto to revoke endpoint
    
    Signed-off-by: Cam Parra <caeparra@gmail.com>
    
    * makes publish optional
    
    Signed-off-by: Cam Parra <caeparra@gmail.com>
    
    * makes changes requested
    
    Signed-off-by: Cam Parra <caeparra@gmail.com>

---
+++ src/api/dtos/revoke.post.req.dto.ts
@@ -0,0 +1,19 @@
+import { IsOptional, IsString, IsBoolean } from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+
+export class RevokePostReqDto {
+
+    @ApiProperty({
+        type: String,
+        description: `Credential exchange ID for the credential that will be revoked`
+    })
+    @IsString() readonly credentialExchangeId: string;
+
+    @ApiPropertyOptional({
+        type: Boolean,
+        description: `Boolean that decides if this revoke credential should this credential be published to the ledger`,
+        default: true
+    })
+    @IsOptional() @IsBoolean() readonly publish: boolean;
+
+}
