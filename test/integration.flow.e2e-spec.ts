c865b72735f0c6f996eda0f43946765674c4a954
Jacob Saur <jsaur@kiva.org>
2021-09-07 15:34:45 +0100

    add test
    Signed-off-by: Jacob Saur <jsaur@kiva.org>

---
+++ test/integration.flow.e2e-spec.ts
@@ -0,0 +1,54 @@
+import request from 'supertest';
+import { Logger } from 'protocol-common/logger';
+import { ProtocolUtility } from 'protocol-common/protocol.utility';
+
+/**
+ * This test goes through the simplified flow of registering an agent, creating schemas, issuing and verifying credentials,etc
+ */
+describe('Tests simplified example flow', () => {
+
+    const sascUrl = 'http://localhost:3030';
+    const mamcUrl = 'http://localhost:3033';
+
+    beforeAll(async () => {
+        jest.setTimeout(20000);
+    });
+
+    it('Register multi agent in multi controller', async () => {
+        const data = {
+            "label": "Multi agent multi controller",
+        }
+        return request(mamcUrl)
+            .post('/v2/api/agent/register')
+            .set('agent', 'mamcagent2')
+            .send(data)
+            .expect((res) => {
+                expect(res.status).toBe(201);
+                expect(res.body.agentId).toBe('mamcagent2');
+            });
+    });
+
+    it('Publicize steward DID on sasc', async () => {
+        const data = {
+            did: 'Th7MpTaRZVRYnPiabds81Y'
+        }
+        return request(sascUrl)
+            .post('/v1/agent/publicize-did')
+            .send(data)
+            .expect((res) => {
+                expect(res.status).toBe(201);
+                expect(res.body.result.did).toBe('Th7MpTaRZVRYnPiabds81Y');
+            });
+    });
+
+    it('Request mamc be registered as endorser', async () => {
+        return request(mamcUrl)
+            .post('/v2/api/request/endorser')
+            .set('agent', 'mamcagent2')
+            .expect((res) => {
+                expect(res.status).toBe(201);
+                expect(res.body.success).toBe(true);
+            });
+    });
+   
+});
