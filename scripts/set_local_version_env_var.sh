02332fddde9a4b99a1a32d0561135d8773899f90
Jeff Kennedy <jeffk@kiva.org>
2021-12-06 14:48:02 -0800

    Don't echo from the script
    Signed-off-by: Jeff Kennedy <jeffk@kiva.org>

--- scripts/set_local_version_env_var.sh
+++ scripts/set_local_version_env_var.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
+# Note: we don't export directly because that will ignore any failures in the jq command
 localVersion=$(jq -r '.version' package.json)
 export LOCAL_VERSION=$localVersion
-echo "$localVersion"
 unset localVersion
