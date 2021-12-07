95b9836858bdaab5a8ad7b39c36ab71dd9eca889
Jeff Kennedy <jeffk@kiva.org>
2021-12-06 16:27:38 -0800

    Remove test - fix worked.
    Signed-off-by: Jeff Kennedy <jeffk@kiva.org>

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -19,6 +19,6 @@ else
   # The .npmrc file is only needed for publish and can cause issues when left around, so manually adding here
   echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > dist/.npmrc
   cd dist
-#  npm publish --verbose
+  npm publish --verbose
   cd ..
 fi
