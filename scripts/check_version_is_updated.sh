9b616afe15feb388fc860334e09bc1225bfb1846
Jeff Kennedy <jeffk@kiva.org>
2021-12-07 07:16:22 -0800

    localVersion -> newVersion. Add another step to test it halts properly.
    Signed-off-by: Jeff Kennedy <jeffk@kiva.org>

--- scripts/check_version_is_updated.sh
+++ scripts/check_version_is_updated.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 # Note: we don't export directly because that will ignore any failures in the jq command
-localVersion=$(jq -r '.version' package.json)
+newVersion=$(jq -r '.version' package.json)
 remoteVersion=$(npm show aries-controller version)
 
-export LOCAL_VERSION=$localVersion
-if [ "$localVersion" == "$remoteVersion" ]; then
+export NEW_VERSION=$newVersion
+if [ "$newVersion" == "$remoteVersion" ]; then
   export VERSION_IS_UPDATED=false
 else
   export VERSION_IS_UPDATED=true
 fi
 
-unset localVersion
+unset newVersion
 unset remoteVersion
--- scripts/set_local_version_env_var.sh
+++ scripts/set_local_version_env_var.sh
@@ -2,5 +2,5 @@
 
 # Note: we don't export directly because that will ignore any failures in the jq command
 localVersion=$(jq -r '.version' package.json)
-export LOCAL_VERSION=$localVersion
+export NEW_VERSION=$localVersion
 unset localVersion
