b2a413abb6d502bcd14610ff5670cd6e46402caf
Jeff Kennedy <69258574+jeffk-kiva@users.noreply.github.com>
2021-12-08 10:24:24 -0800

    Fix npm token issue [CI/CD] (#136)
    * Fix publish script to just refer to the NPM_TOKEN environment variable without actually filling in the value.
    * Publish packages and push to dev in a single job. Otherwise, it's difficult to detect in separate jobs if there has been a version update.
    * Increment version to 1.0.88
    
    Signed-off-by: Jeff Kennedy <jeffk@kiva.org>

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -11,7 +11,9 @@ echo "Publishing $version"
 cp package.json dist/
 cp README.md dist/
 # The .npmrc file is only needed for publish and can cause issues when left around, so manually adding here
-echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > dist/.npmrc
+# Note: This intentionally uses single-quotes in order to not put the actual value of NPM_TOKEN into .npmrc. Rather,
+# we can rely on the npm cli to interpret this appropriately.
+echo '//registry.npmjs.org/:_authToken=${NPM_TOKEN}' > dist/.npmrc
 cd dist
 npm publish --verbose
 cd ..
